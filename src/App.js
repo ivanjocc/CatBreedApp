3cba05f6d8b8089d79dfac4ea2e7f551a5ff0007
Iván <ivanjose_04@hotmail.com>
2023-08-22 12:52:53 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './styles/App.css';
+import BreedList from './components/BreedList';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Cat Breeds App</h1>
       </header>
+      <main>
+        <BreedList />
+      </main>
     </div>
   );
 }
